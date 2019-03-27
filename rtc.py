b5eb6ed061ef05bc7245d7f48f5c649b4065c804
Ian Dixon <iandixon@staff-dhcp61.asf.alaska.edu>
2019-03-27 15:00:06 -0800

    cleanup junk.

--- rtc.py
+++ rtc.py
@@ -44,23 +44,29 @@ f.close()
 local_file = download_file(download_url)
 
 subprocess.run(["/usr/local/snap/bin/gpt", "Apply-Orbit-File", "-Ssource=" + local_file, "-t",  "Orb"])
+os.unlink(local_file)
+
 subprocess.run(["/usr/local/snap/bin/gpt", "Calibration", "-PoutputBetaBand=true", "-PoutputSigmaBand=false", "-Ssource=Orb.dim", "-t", "Cal"])
 os.unlink("Orb.dim")
 shutil.rmtree("Orb.data")
+
 subprocess.run(["/usr/local/snap/bin/gpt", "Terrain-Flattening", "-PdemName=SRTM 1Sec HGT", "-PreGridMethod=False", "-Ssource=Cal.dim", "-t", "TF"])
 os.unlink("TF.dim")
 os.unlink("Cal.dim")
+
 subprocess.run(["/usr/local/snap/bin/gpt", "Terrain-Correction", "-PpixelSpacingInMeter=30.0", "-PmapProjection=EPSG:32613", "-PdemName=SRTM 1Sec HGT", "-Ssource=TF.dim", "-t", "TC"])
 os.unlink("TC.dim")
 shutil.rmtree("Cal.data")
+
 subprocess.run(["gdal_translate", "-of", "GTiff", "-a_nodata", "0", "TC.data/Gamma0_VH.img", "VH.tif"])
 subprocess.run(["gdal_translate", "-of", "GTiff", "-a_nodata", "0", "TC.data/Gamma0_VV.img", "VV.tif"])
 os.unlink("TC.dim")
 shutil.rmtree("TC.data")
+
 subprocess.run(["gdaladdo", "-r", "average", "VH.tif", "2", "4", "8", "16"])
 subprocess.run(["gdaladdo", "-r", "average", "VV.tif", "2", "4", "8", "16"])
 
-subprocess.run(["gdal_translate", "-c", "TILED=YES", "-co", "COMPRESS=DEFLATE", "-co", "COPY_SRC_OVERVIEWS=YES", "VH.tif", "/output/" + args.granule + "_vh.tif"])
-subprocess.run(["gdal_translate", "-c", "TILED=YES", "-co", "COMPRESS=DEFLATE", "-co", "COPY_SRC_OVERVIEWS=YES", "VV.tif", "/output/" + args.granule + "_vh.tif"])
-os.unlink("VV.TIF")
-os.unlink("VH.TIF")
+subprocess.run(["gdal_translate", "-co", "TILED=YES", "-co", "COMPRESS=DEFLATE", "-co", "COPY_SRC_OVERVIEWS=YES", "VH.tif", "/output/" + args.granule + "_vh.tif"])
+subprocess.run(["gdal_translate", "-co", "TILED=YES", "-co", "COMPRESS=DEFLATE", "-co", "COPY_SRC_OVERVIEWS=YES", "VV.tif", "/output/" + args.granule + "_vh.tif"])
+os.unlink("VV.tif")
+os.unlink("VH.tif")
