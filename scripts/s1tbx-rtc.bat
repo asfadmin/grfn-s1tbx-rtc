ff07699cf283c68152ba5fb06f0aa0ee6a55996f
Andrew Johnston <asjohnston@alaska.edu>
2019-05-02 18:04:49 +0000

    add --cpus and --memory in convenience scripts

--- scripts/s1tbx-rtc.bat
+++ scripts/s1tbx-rtc.bat
@@ -3,5 +3,5 @@ echo Checking for updates
 docker pull asfdaac/s1tbx-rtc
 echo.
 set /p GRANULE="Granule: "
-docker run -it -v %CD%:/output --rm asfdaac/s1tbx-rtc --granule %GRANULE%
+docker run -it --cpus=0 --memory 16g -v %CD%:/output --rm asfdaac/s1tbx-rtc --granule %GRANULE%
 @pause
