06027e130a3a76d4659a77860610e5ac0c2f6056
Andrew Johnston <asjohnston@alaska.edu>
2019-05-07 17:28:15 +0000

    add helper .sh script and update script permissions

---
+++ scripts/s1tbx-rtc.sh
@@ -0,0 +1,7 @@
+echo Checking for updates
+docker pull asfdaac/s1tbx-rtc
+echo
+read -p "Granule: " GRANULE
+docker run -it --cpus=0 --memory 16g -v $(pwd):/output --rm asfdaac/s1tbx-rtc --granule $GRANULE
+echo
+read -p "Press any key to continue"
