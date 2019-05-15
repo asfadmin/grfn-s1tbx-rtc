447d66054cffe12180bc1299bc5b8392ed46c80b
idixon <irdixon@alaska.edu>
2019-05-15 15:34:52 -0800

    Delete s1tbx-rtc.bat

--- scripts/s1tbx-rtc.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Checking for updates
-docker pull asfdaac/s1tbx-rtc
-echo.
-set /p GRANULE="Granule: "
-docker run -it --cpus=0 --memory 16g -v %CD%:/output --rm asfdaac/s1tbx-rtc --granule %GRANULE%
-@pause
